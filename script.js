75fb6d15cd870466195319bff3bc2ec3bbf0755e
Saabbass <178414@novacollege.nl>
2026-01-12 09:45:32 +0100

    feat: added header and first section

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("navbar").style.padding = "0px 0px";
+    document.getElementById("navbar").style.background = "#1b4566";
+    document.getElementById("navbar").style.borderRadius = "0 0 25px 25px";
+    document.getElementById("navbar").style.borderBottom = "1px solid #ffa500";
+  } else {
+    document.getElementById("navbar").style.padding = "10px 0px";
+    document.getElementById("navbar").style.background = "#254a66";
+    document.getElementById("navbar").style.borderRadius = "0";
+    document.getElementById("navbar").style.borderBottom = "none";
+  }
+}
